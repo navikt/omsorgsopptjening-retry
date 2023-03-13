ac315730b2f4915cf04f2c0058cca610f7aed7e2
Mo Amini <mo.amini@nav.no>
2023-03-13 10:08:03 +0100

    Legger til litt mer logging i start / stopp mekanismen
    Co-authored-by: eivind.askestad@nav.no

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/retry/schedule/ScheduleKafkaListener.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/retry/schedule/ScheduleKafkaListener.kt
@@ -15,12 +15,14 @@ class ScheduleKafkaListener(
     fun startListener() {
         LOGGER.info("Starter omsorgsarbeidListener ${LocalTime.now()}")
         kafkaListenerEndpointRegistry.getListenerContainer("omsorgsarbeidListener")!!.start()
+        LOGGER.info("Finished with start scheduler ${LocalTime.now()}")
     }
 
     @Scheduled(cron = "0 40 15 * * ?")
     fun stopListener() {
         LOGGER.info("Stopper omsorgsarbeidListener ${LocalTime.now()}")
         kafkaListenerEndpointRegistry.getListenerContainer("omsorgsarbeidListener")!!.stop()
+        LOGGER.info("Finished with stop scheduler ${LocalTime.now()}")
     }
 
     companion object {
